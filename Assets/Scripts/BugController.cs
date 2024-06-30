491e4598276444554619884aecf4f5da1b923832
arboos <151074202+arboos@users.noreply.github.com>
2024-06-30 15:59:54 +0300

    Bug controller merge

--- Assets/Scripts/BugController.cs
+++ Assets/Scripts/BugController.cs
@@ -20,7 +20,7 @@ public class BugController : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            StartCoroutine(SpawnBugs(4, spawnSpeed));
+            StartCoroutine(SpawnBugs(bugNums.Count, spawnSpeed));
         }
     }
 
